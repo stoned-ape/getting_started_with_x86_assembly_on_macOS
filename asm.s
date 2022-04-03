05e0e2ea6deb12028ec89405a126a7ef89e17dca
stoned-ape <samuraiheadhunter@gmail.com>
2022-04-03 01:32:48 -0500

    _

--- asm.s
+++ asm.s
@@ -1,9 +1,11 @@
 .global _print_string
 
 _print_string:
-	mov $0x2000004,%rax ;we want to use the "write" system call
-	mov %rsi,%rdx ;pass the string as the second argument
-	mov %rdi,%rsi ;pass the length of the string as the third argument
-	mov $1,%rdi ;pass standard out as the first argument
-	syscall ;make the system call
-	ret ;return from the function
+	mov $0x2000004,%rax 
+	mov %rsi,%rdx 
+	mov %rdi,%rsi 
+	mov $1,%rdi 
+	syscall 
+	ret 
+
+
--- main.c
+++ main.c
@@ -3,3 +3,8 @@ extern void print_string(char *s,int length);
 int main(){
 	print_string("hello world\n",12);
 }
+
+
+
+
+
